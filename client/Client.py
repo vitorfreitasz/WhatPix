13557168bed047e3b1c5784dba2d60736f32f9a3
Samuel Lemos <samueltdelemos@gmail.com>
2024-08-06 18:55:27 -0300

    chore: adicionando alguns comentários no código do client

--- client/Client.py
+++ client/Client.py
@@ -6,17 +6,16 @@ class Client:
         self.port = port
         self.socket = socket(AF_INET, SOCK_STREAM)
 
-    def start(self):
+    def start(self): # se conecta com o servidor
         self.socket.connect((self.host, self.port))
         print(f'Connected on: {self.host}:{self.port}')
         print("\n\nConnected!!!\n\n")
         self.messages()
 
-    def close(self):
+    def close(self): # encerra a conexão com o servidor
         self.socket.close()
 
     def messages(self):
         while True:
             data = self.socket.recv(1024)
             print(f"{data}")
-
