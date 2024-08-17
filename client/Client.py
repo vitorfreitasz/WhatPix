e4f54c87e1ff2e461fa9191faab2d37b386b2a47
Samuel Lemos <samueltdelemos@gmail.com>
2024-08-17 14:29:21 -0300

    feat: adicionando prints dos comando para o usuário

--- client/Client.py
+++ client/Client.py
@@ -35,29 +35,34 @@ class Client:
     def close(self): # encerra a conexão com o servidor
         self.socket.close()
         
-    def registerOrLogin(self):
+    def registerOrLogin(self): # pede para o usuário que se registre ou faça login
         while True:
             response = int(input('--------------------------------------\n\n Para cadastrar-se, digite 0.\n\n Para entrar digite 1.\n\n--------------------------------------\n\n'))
 
-            if response == 0:
+            if response == 0: # envia um código 01 para o servidor solicitando um novo cadastro 
                 self.socket.send("01".encode('utf-8'))
                 return
-            elif response == 1:
+            elif response == 1: # pede para o usuário digitar seu número identificador
                 while True:
                     id = str(input(f' Escreva o seu código identificador: '))
                     if len(id) == 13:
-                        self.socket.send(f"03{id}".encode('utf-8'))
+                        self.socket.send(f"03{id}".encode('utf-8')) # envia um código 03 e o id do usuário para o servidor solcitando login
                         return
                     else:
                         print("Erro: código identificador de tamanho irregular.")
         
-    def awaitingComands(self):
+    def awaitingComands(self): # espera por um comando do usuário para tomar uma ação
         while True:
+            print("Lista de comandos:")
+            print("/m -> Solicita o id do usuário que deseja enviar uma mensagem;")
+            print("/r -> Envia mensagem para o usuário com o id da última mensagem recebida;")
+            print("cancelar -> Cancela o envio de mensagem;")
+            print("/dc -> Desconecta do servidor;")
+            print('\n Escreva seu comando: ')
             comand = input()
             self.handleComand(comand)
-        return
             
-    def handleComand(self, comand):
+    def handleComand(self, comand): # verifica qual o comando o usuário digitou e toma uma ação com base nisso
         if comand == '/m':
             while True:
                 dest = str(input(f"Para quem deseja enviar (digite 'cancelar' para cancelar): "))
@@ -84,6 +89,8 @@ class Client:
             else:
                 print("Não há ninguém para responder.")
         if comand == '/dc':
+            print("\n Desconectando do servidor...")
+            time.sleep(2)
             self.socket.close()
         return
     
@@ -97,7 +104,6 @@ class Client:
             if req[:2] == "02":
                 self.codeUser = req[2:]
                 print(f"\nUsuário cadastrado!\n Id: ({self.codeUser})\n")
-                print('\n Escreva seu comando: ')
                 thread = threading.Thread(target=self.awaitingComands)
                 thread.start()
             if req[:2] == "04":
